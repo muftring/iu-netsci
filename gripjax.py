5ca97015ff07d99e87b623be631d6b7eed9c0eb4
Michael Uftring <michael.uftring@gmail.com>
2018-04-13 15:08:40 -0400

    Exam: (prep) downgraded grip to v4.3.2, added config for inline LaTeX math

--- gripjax.py
+++ gripjax.py
@@ -4,6 +4,14 @@ from grip import GitHubRenderer, Grip
 
 MATHJAX_SCRIPT = """
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS-MML_HTMLorMML-full"></script>
+<script type="text/javascript">
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes: true
+  }
+});
+</script>
 <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="javascript:MathJax.Hub.Queue(['Typeset', MathJax.Hub]);">
 """
 
